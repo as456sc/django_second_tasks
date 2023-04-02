171d8dc0fca0f271026f4bdc81f750c12a8a3646
Dmitry <kokovin.02@inbox.ru>
2023-04-02 22:21:46 +0300

    first commit

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
+
+
 
 def index(request):
     return redirect('catalog')
 
 
+
+
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    sort_pages = request.GET.get('sort')
+    phones = Phone.objects.all()
+    if sort_pages == 'low':
+        phones = phones.order_by('price')
+    elif sort_pages == 'high':
+        phones = phones.order_by('-price')
+    elif sort_pages == 'alph':
+        phones = phones.order_by('name')
+    context = {'phones': phones,
+               }
+    return render(request, template, context=context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = Phone.objects.filter(slug__contains=slug)
+    context = {'phone': phone}
+    return render(request, template, context=context)
